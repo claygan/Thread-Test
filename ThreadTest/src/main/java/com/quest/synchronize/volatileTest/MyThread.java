d11cf78c5a46638305253ed55597435ff5a4ce7b
ganshimin <ganshimin@zhongzhihui.com>
2017-07-03 09:46:33 +0800

    test

--- ThreadTest/src/main/java/com/quest/synchronize/volatileTest/MyThread.java
+++ ThreadTest/src/main/java/com/quest/synchronize/volatileTest/MyThread.java
@@ -2,6 +2,7 @@ package com.quest.synchronize.volatileTest;
 
 public class MyThread extends Thread {
 	volatile public static int count;//如果使用synchronized修饰，这里的volatile就没有必要使用了
+	volatile public static int[] array;//如果volatile修饰数组，只针对引用有作用，但是对元素不能起到保护作用
 	synchronized private static void addCount(){
 		for(int i=0;i<100;i++){
 			count++;
--- ThreadTest/src/test/java/com/quest/ThreadTest/AppTest.java
+++ ThreadTest/src/test/java/com/quest/ThreadTest/AppTest.java
@@ -1,38 +1,21 @@
 package com.quest.ThreadTest;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+		@Test
+		public void test(){
+			String link = "/index.htm";
+//			link = link.substring(0, link.lastIndexOf("/")) +"/**/*";
+			System.out.println(link);
+			System.out.println(link.indexOf("index"));
+//			if(link.indexOf("index")){
+//				System.out.println("success");
+//			}
+			
+		}
 }
